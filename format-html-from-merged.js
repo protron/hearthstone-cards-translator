6c4ba1ccc4d4c53619ead17890e4ec6086579b6e
Protron MaDiSoN <protronm@gmail.com>
2015-08-16 06:19:38 -0300

    First working version (using jade)

--- format-html-from-merged.js
+++ format-html-from-merged.js
@@ -1,16 +1,20 @@
+var jade = require('jade');
 var fs = require('fs');
 var _ = require('lodash');
+var path = require('path');
 
-var inputFile = "output/merged.min.json";
-var outputFileTranslations = "output/translations.min.json";
-var outputFileInputLangNames = "output/inputLangNames.min.json";
+var inputFileJson = "output/merged.min.json";
+var inputFileJade = "autocomplete.jade";
+var outputFileTranslations = "output/translations.js";
+var outputFileOptions = "output/options.js";
+var outputFileHtml = "output/autocomplete.htm";
 
 function filterCards(parsedData) {
   return _.filter(parsedData, function(v) {
     return v.type !== 'Hero Power' && v.type !== 'Enchantment' && v.deckSet !==
       'Debug' && v.id.split('_')[0] !== 'GAME';
   });
-};
+}
 
 function getNameTranslations(parsedData) {
   return _.reduce(parsedData, function(result, v, key) {
@@ -18,25 +22,63 @@ function getNameTranslations(parsedData) {
     result[m[1]] = m[0];
     return result;
   }, {});
-};
+}
 
-function writeToFile(filePath, content) {
+function formatOutputOptions(optionNames) {
+  return 'var cardOptions = ' + JSON.stringify(optionNames);
+}
+
+function formatOutputTranslations(nameTranslations) {
+  return 'var nameTranslations = ' + JSON.stringify(nameTranslations);
+}
+
+function writeToFile(filePath, content, done) {
   console.log("Writing: " + filePath);
   var stream = fs.createWriteStream(filePath);
   stream.once('open', function(fd) {
     stream.write(content);
     stream.end();
+    done();
   });
 }
 
-console.log("Reading: " + inputFile);
-fs.readFile(inputFile, function(err, data) {
-  if (err) throw err;
-  var parsedData = JSON.parse(data);
-  var cards = filterCards(parsedData);
-  var nameTranslations = getNameTranslations(cards);
-  writeToFile(outputFileTranslations, JSON.stringify(nameTranslations));
-  var optionNames = _.keys(nameTranslations);
-  writeToFile(outputFileInputLangNames, JSON.stringify(optionNames));
-  console.log("DONE!");
-});
+function copyFile(input, output, done) {
+  fs.createReadStream(input)
+    .pipe(fs.createWriteStream(output))
+    .on('finish', done);
+}
+
+function compileJade() {
+  console.log("Compiling: " + inputFileJade);
+  var tempJadePath = path.resolve(__dirname, 'output', inputFileJade);
+  copyFile(inputFileJade, tempJadePath, function() {
+    var fn = jade.compileFile(tempJadePath, {});
+    var html = fn();
+    fs.writeFileSync(outputFileHtml, html);
+    fs.unlinkSync(tempJadePath);
+    console.log("DONE!");
+  });
+}
+
+var state = {
+  nameTranslations: null,
+
+  on1stOutputReady: function() {
+    var optionNames = _.keys(this.nameTranslations);
+    writeToFile(outputFileOptions, formatOutputOptions(optionNames),
+      compileJade);
+  },
+
+  readJsonInput: function(err, data) {
+    if (err) throw err;
+    var parsedData = JSON.parse(data);
+    var cards = filterCards(parsedData);
+    this.nameTranslations = getNameTranslations(cards);
+    writeToFile(outputFileTranslations, formatOutputTranslations(
+      this.nameTranslations), this.on1stOutputReady);
+  }
+};
+_.bindAll(state, 'readJsonInput', 'on1stOutputReady')
+
+console.log("Reading: " + inputFileJson);
+fs.readFile(inputFileJson, state.readJsonInput);
